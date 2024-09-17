8a223dce395c424848c9c643f44113b0c332e176
Neto <wantedshark34@gmail.com>
2024-09-16 21:43:23 -0300

    icons added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
-import Button from "../button/Button";
-import logo from "../../images/logo.png"
-import "./navbar.css"
-function HandleClick() {
-  console.log("clicou");
-}
+import { RiLoginBoxLine, RiLogoutBoxRLine } from "react-icons/ri";
+import { AiOutlineLogout,AiOutlineLogin } from "react-icons/ai";
+import logo from "../../images/logo.png";
+import "./navbar.css";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [islogged, setIslogged] = useState(false);
+
+  function handleClic() {
+    setIslogged(!islogged)
+  }
   return (
     <div className="navbar">
       <div className="navbar_logo">
@@ -18,7 +22,17 @@ export default function Navbar() {
         <Link to="/newTask">new task</Link>
       </div>
       <div className="navbar_footer">
-        <Button text={"Logout"} handleClick={HandleClick} />
+        {!islogged ? (
+          <button className="navbar_footer_button" onClick={handleClic}>
+            <span>Login</span>
+            <AiOutlineLogin size={20} />
+          </button>
+        ) : (
+          <button className="navbar_footer_button" onClick={handleClic}>
+            <span>Logout</span>
+            <AiOutlineLogout size={20} />
+          </button>
+        )}
       </div>
     </div>
   );

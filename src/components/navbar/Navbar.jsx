6fea7355841d852553c202c3a4213c526fbc37fb
Neto <wantedshark34@gmail.com>
2024-09-21 15:29:04 -0300

    navbar created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { RiLoginBoxLine, RiLogoutBoxRLine } from "react-icons/ri";
 import { AiOutlineLogout,AiOutlineLogin } from "react-icons/ai";
 import logo from "../../images/logo.png";
 import "./navbar.css";
@@ -18,15 +17,22 @@ export default function Navbar() {
           <img src={logo} className="navbar_logo_img" alt="logo" />
         </Link>
       </div>
-      <div className="navbar_links">
-        <Link to="/newTask">new task</Link>
+      <div className="navbar_menu">
+        <ul className="navbar_list">
+          <li className="navbar_list_item">
+            <Link to="/newTask">new task</Link>
+          </li>
+         { islogged && <li className="navbar_list_item">
+            <Link to="/users">users</Link>
+          </li>}
+        </ul>
       </div>
       <div className="navbar_footer">
         {!islogged ? (
-          <button className="navbar_footer_button" onClick={handleClic}>
+          <Link to="/login" className="navbar_footer_button" onClick={handleClic}>
             <span>Login</span>
             <AiOutlineLogin size={20} />
-          </button>
+          </Link>
         ) : (
           <button className="navbar_footer_button" onClick={handleClic}>
             <span>Logout</span>
